8a073151aca359ae439fc2be33f6fd132cd6a68e
Kirill Artamonov <artamonov.kirill@gmail.com>
2020-08-09 21:56:30 +0200

    :recycle: method references instead of closures

--- e2e/localstack/src/test/groovy/Process.groovy
+++ e2e/localstack/src/test/groovy/Process.groovy
@@ -4,8 +4,8 @@ class Process {
     static run(String command) {
         def process = Runtime.runtime.exec(command)
 
-        def out = join(process.inputStream, { System.out.println(it) })
-        def error = join(process.errorStream, { System.err.println(it) })
+        def out = join(process.inputStream, System.out.&println)
+        def error = join(process.errorStream, System.err.&println)
 
         process.waitFor()
         [exitValue: process.exitValue(), out: out, error: error]
